516b32d4f3450bb7729f0b4bfa8e0c7d26474617
Felipe Santa Cruz <91490113+Felipewiiu@users.noreply.github.com>
2023-11-30 19:50:38 -0300

    cors alteração

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ import db from "./config/dbConect.js";
 import routes from "./routes/index.js";
 import manipuladorDeErros from "./middlewares/manipuladorDeErros.js";
 import manipulador404 from "./middlewares/manipulador404.js";
-var cors = require('cors')
+// var cors = require('cors');
+import cors from "cors";
 
 db.on("error", console.log.bind(console, "Erro de conexão")); // aiso de erro de conexão
 db.once("open", () => {
@@ -13,7 +14,7 @@ db.once("open", () => {
 const app = express();
 
 app.use(express.json()); // isso transforma o que é recebido em json
-app.use(cors())
+app.use(cors());
 
 // eslint-disable-next-line no-unused-vars
 app.get("/livros", (req, res, next) => {
